4e88d2d39b86a975267f8b6ae4d3bb26afa7e77d
Tibboh17 <121493257+Tibboh17@users.noreply.github.com>
2024-07-18 11:43:48 +0900

    [level 0] Title: 왼쪽 오른쪽, Time: 0.01 ms, Memory: 10.3 MB -BaekjoonHub

---
+++ 프로그래머스/0/181890. 왼쪽 오른쪽/왼쪽 오른쪽.py
@@ -0,0 +1,8 @@
+def solution(str_list):
+    if len(str_list) == 1:
+        return []
+    for i in range(len(str_list)):
+        if str_list[i] == "l":
+            return str_list[:i]
+        elif str_list[i] == "r":
+            return str_list[i + 1:]
